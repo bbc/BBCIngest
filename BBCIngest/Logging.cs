ca8fe8c8e3593bf1f2fea2d595e539d938ce712c
Julian <Julian.cable@bbc.co.uk>
2016-11-26 10:29:50 +0000

    Convert logging to use delegates.

--- BBCIngest/Logging.cs
+++ BBCIngest/Logging.cs
@@ -5,8 +5,11 @@ using System.Text;
 
 namespace BBCIngest
 {
+    public delegate void LogDelegate(string s);
+
     class Logging
     {
+        private event LogDelegate logevent;
         private AppSettings conf;
         private HttpClient hc;
 
@@ -14,18 +17,30 @@ namespace BBCIngest
         {
             this.conf = conf;
             this.hc = hc;
+            this.logevent += new LogDelegate(write);
+            this.logevent += new LogDelegate(post);
         }
 
         internal void WriteLine(string logmessage)
+        {
+            logevent(logmessage);
+        }
+
+        internal void write(string logmessage)
         {
             DateTime dt = DateTime.UtcNow;
             StreamWriter log = System.IO.File.AppendText(conf.Logfolder + conf.Basename + ".log");
             log.WriteLine(dt.ToString() + " "+ logmessage + " by " + conf.Station + " in " + conf.City);
             log.Dispose();
+        }
+
+        internal void post(string logmessage)
+        {
+            DateTime dt = DateTime.UtcNow;
             if (conf.PostLogs && conf.LogUrl != "")
             {
                 string f = "\"date\": \"{0:yyyy-MM-ddTHH:mm:ssZ}\", \"message\": \"{1}\", \"city\": \"{2}\", \"station\": \"{3}\"";
-                string jsonObject = "[{"+string.Format(f, dt, logmessage, conf.City, conf.Station)+"}]";
+                string jsonObject = "[{" + string.Format(f, dt, logmessage, conf.City, conf.Station) + "}]";
                 var content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
                 hc.PostAsync(conf.LogUrl, content);
             }
