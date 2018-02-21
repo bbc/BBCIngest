201257e167f595551089b2daa55fcb7ebd3049f3
Julian Cable <julian.cable@yahoo.com>
2018-02-21 12:10:48 +0000

    Add files via upload
    Add a parameter to the TaskName if a custom settings is in use.

--- Ingest/Schedule.cs
+++ Ingest/Schedule.cs
@@ -5,6 +5,7 @@ namespace Ingest
 {
     public interface IScheduleSettings
     {
+        string TaskName { get; set; }
         string Minutepattern { get; set; }
         string Hourpattern { get; set; }
         int MinutesBefore { get; set; }
