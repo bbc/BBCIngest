b17d3b92d05bc24968e13133c02bae5c310bc359
Julian <Julian.cable@bbc.co.uk>
2018-02-21 21:03:18 +0000

    new tests

---
+++ IngestTests/FetchTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ingest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Ingest.Tests
+{
+    [TestClass()]
+    public class FetchTests
+    {
+        [TestMethod()]
+        public void FetchTest()
+        {
+        }
+
+        [TestMethod()]
+        public void addTerseMessageListenerTest()
+        {
+        }
+
+        [TestMethod()]
+        public void addChattyMessageListenerTest()
+        {
+        }
+
+        [TestMethod()]
+        public void addEditionListenerTest()
+        {
+        }
+
+        [TestMethod()]
+        public void addLogListenerTest()
+        {
+        }
+
+        [TestMethod()]
+        public void webnameTest()
+        {
+            AppSettings settings = new AppSettings();
+            settings.Basename = "y";
+            settings.Prefix = "http://x/";
+            settings.Suffix = "wav";
+            settings.Webdate = "";
+            HttpClient hc = new HttpClient();
+            Fetch uut = new Fetch(settings, hc);
+            DateTime t = DateTime.Now;
+            string s = uut.webname(t);
+        }
+
+        [TestMethod()]
+        public void lastWeHaveTest()
+        {
+        }
+
+        [TestMethod()]
+        public void waitforTest()
+        {
+        }
+
+        [TestMethod()]
+        public void saveTest()
+        {
+        }
+
+        [TestMethod()]
+        public void latestPublishTimeTest()
+        {
+        }
+
+        [TestMethod()]
+        public void shouldRefetchTest()
+        {
+        }
+
+        [TestMethod()]
+        public void reFetchIfNeededTest()
+        {
+        }
+    }
+}
