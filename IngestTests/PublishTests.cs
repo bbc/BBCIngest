3891bba4c96d89bfdd45a59168f796788f7ab664
Julian <Julian.cable@bbc.co.uk>
2018-02-21 21:59:12 +0000

    Fix tests

--- IngestTests/PublishTests.cs
+++ IngestTests/PublishTests.cs
@@ -14,10 +14,13 @@ namespace Ingest.Tests
         {
             DateTime t = new DateTime(2017, 1, 1, 0, 0, 0);
             IPublishSettings conf = new AppSettings();
-            conf.Basename = "test";
+            conf.PublishName = "test";
             conf.Discdate = "HHmm";
+            conf.PublishFormat = "mp3";
             Publish uut = new Publish(conf);
-            Assert.AreEqual(conf.Basename+"0000."+conf.PublishFormat, uut.discname(t));
+            string expected = conf.PublishName + "0000." + conf.PublishFormat;
+            string result = uut.discname(t);
+            Assert.AreEqual(expected, result);
         }
 
         [TestMethod()]
@@ -29,7 +32,7 @@ namespace Ingest.Tests
             Publish uut = new Publish(conf);
             ProcessStartInfo startInfo = uut.getPSI("1", "2");
             Assert.AreEqual("ffmpeg.exe", startInfo.FileName);
-            Assert.AreEqual("-i 1 -ar 44100 -b 256k -acodec libtwolame -f mp2 2", startInfo.Arguments);
+            Assert.AreEqual("-i 1 -af aresample=osr=44100:filter_size=256 -b:a 384k -acodec libtwolame -f mp2 2", startInfo.Arguments);
             startInfo.FileName = Directory.GetCurrentDirectory()+@"\ffmpeg.exe";
             uut.encodeMP2(startInfo);
         }
